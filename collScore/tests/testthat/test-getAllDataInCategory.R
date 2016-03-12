d66b9da80b0a65315a8d86af0beb729bf94b8fd6
Iliana Voynichk <Iliana.voynichka@schneider-electric.com>
2016-03-12 16:48:36 -0500

    Added tests to the package
    Added test-completionRate
    Added test-getAllDataInCategory

---
+++ collScore/tests/testthat/test-getAllDataInCategory.R
@@ -0,0 +1,11 @@
+context("getAllDataInCategory Validation")
+
+test_that("getAllDataInCategory validates the year argument correctly", {
+  expect_error(getAllDataInCategory(categoryName = "earnings", year = 2014, pattern = "", addParams = "id,school.name"), "Incorrect year selection")
+  expect_error(getAllDataInCategory(categoryName = "earnings", year = 1995, pattern = "", addParams = "id,school.name"), "Incorrect year selection")
+})
+
+test_that("getAllDataInCategory validates the categoryName argument correctly", {
+  expect_error(getAllDataInCategory(categoryName = "earning", year = 2013, pattern = "", addParams = "id,school.name"), "Incorrect categoryName")
+  expect_error(getAllDataInCategory(categoryName = "random category", year = 1996, pattern = "", addParams = "id,school.name"), "Incorrect categoryName")
+})
