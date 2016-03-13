478d6e804e1c8adc6ed85f924f07e8fa4a088e80
Iliana Voynichk <Iliana.voynichka@schneider-electric.com>
2016-03-13 16:57:15 -0400

    Modified repaymentRateByIncome and added test
    For repaymentRateByIncome:
    added error handling
    added comments and cleaned up

--- collScore/R/repaymentRateByIncome.R
+++ collScore/R/repaymentRateByIncome.R
@@ -15,6 +15,43 @@
 #' @export
 repaymentRateByIncome <- function(apiKey, dataset, year = 2013, schoolNames, repaymentYears = 3) {
   
+  #..................Helper Functions.............................................................
+  # Helper function to get data based on school name if data for the specified school is available
+  getDataPerSchool <- function(schoolName) {
+    schoolData <- subset(meltedData, meltedData$name == schoolName)
+    if(nrow(schoolData) == 0){
+      message(paste("No data is available for the selected school", schoolName, "therefore data for this school will not be displayed", sep = " "))
+    }
+    else {
+      schoolData
+    }
+  }
+  
+  # Splits a string and returns the last element
+  getLast <- function(x) {
+    res <- unlist(strsplit(x, ".", fixed = TRUE))
+    res[length(res)]
+  }
+  
+  # Subset the data by year if data is available for the supplied year and plot the data
+  doPlotbyYear <- function(x) {
+    plot1 <- NULL
+    dataByYear <- subset(meltedData, year == x)
+    if(all(is.na(dataByYear$value))) {
+      message(paste("There is no data available for the selected year", as.character(x)))
+    }
+    else {
+      plot1 <- ggplot2::ggplot(data = dataByYear, ggplot2::aes(x = dataByYear$income, y = dataByYear$value, fill=dataByYear$name)) + 
+        ggplot2::geom_bar(stat = 'identity', position=ggplot2::position_dodge()) +
+        ggplot2::scale_fill_brewer(palette = "Pastel1") +
+        ggplot2::ggtitle(paste(repaymentYears, "Years Repayment Rate", "For Different Income Brackets", sep = " ")) +
+        ggplot2::labs(x="Income Bracket", y="Repayment Rate (%)", fill='School Name') 
+    }
+    plot1
+  }
+  #...............................................................................................
+  
+  # Data obtained through the REST API and data provided with the package use different variable names
   if (missing(apiKey)) {
     addParams <- "name"
   }
@@ -26,51 +63,30 @@ repaymentRateByIncome <- function(apiKey, dataset, year = 2013, schoolNames, rep
   repaymentData <- getAllDataInCategory(dataset = dataset, categoryName = "repayment", year = year, pattern = "suppressed.income", addParams = addParams)
   meltedData <- reshape2::melt(repaymentData, id.vars = addParams)
   
-  if(length(schoolNames) > 1) {
-    temp <- lapply(schoolNames, function(x) subset(meltedData, meltedData$name == x))
+  temp <- lapply(schoolNames, getDataPerSchool)
+  if(!is.null(temp[[1]])) {
     meltedData <- do.call(rbind, temp)
-  }
-  
-  helpF <- function(x) {
-    res <- unlist(strsplit(x, ".", fixed = TRUE))
-    res[length(res)]
-  }
-  
-  columnYear <- unlist(lapply(as.character(meltedData$variable), function(x) unlist(strsplit(x, ".", fixed = TRUE))[1]))
-  columnIncome <- unlist(lapply(as.character(meltedData$variable), helpF))
-  columnRate <- unlist(lapply(as.character(meltedData$variable), function(x) unlist(strsplit(x, ".", fixed = TRUE))[3]))
-  
-  meltedData$year <- columnYear
-  meltedData$income <- columnIncome
-  meltedData$repaymentRate <- columnRate
-  meltedData$variable <- NULL
-  
-  repaymentR <- meltedData$repaymentRate %in% grep(as.character(repaymentYears), meltedData$repaymentRate, value = TRUE)
-  meltedData <- subset(meltedData, repaymentR)
-
-  #colnames(meltedData)[colnames(meltedData) == field] <- addParams
-  meltedData$value <- as.numeric(meltedData$value)
-  meltedData <- meltedData[with(meltedData, order(income)), ]
-  
-  doPlotbyYear <- function(x) {
-    plot1 <- NULL
-    dataByYear <- subset(meltedData, year == x)
-    if(all(is.na(dataByYear$value))) {
-      warning(paste("There is no data available for the selected year", as.character(x)))
+    columnYear <- unlist(lapply(as.character(meltedData$variable), function(x) unlist(strsplit(x, ".", fixed = TRUE))[1]))
+    columnIncome <- unlist(lapply(as.character(meltedData$variable), getLast))
+    columnRate <- unlist(lapply(as.character(meltedData$variable), function(x) unlist(strsplit(x, ".", fixed = TRUE))[3]))
+    meltedData$year <- columnYear
+    meltedData$income <- columnIncome
+    meltedData$repaymentRate <- columnRate
+    meltedData$variable <- NULL
+    repaymentR <- meltedData$repaymentRate %in% grep(as.character(repaymentYears), meltedData$repaymentRate, value = TRUE)
+    meltedData <- subset(meltedData, repaymentR)
+    meltedData$value <- as.numeric(meltedData$value)
+    meltedData <- meltedData[with(meltedData, order(income)), ]
+    myPlots <-  Filter(Negate(is.null), lapply(year, doPlotbyYear))
+    if(length(myPlots) > 1) {
+      numCols <- ceiling(length(myPlots)/2)
+      multiplot(plotlist = myPlots, cols = numCols, byrow=TRUE)
     }
     else {
-      plot1 <- ggplot2::ggplot(data = dataByYear, ggplot2::aes(x = dataByYear$income, y = dataByYear$value, fill=dataByYear$name)) + 
-        ggplot2::geom_bar(stat = 'identity', position=ggplot2::position_dodge()) + 
-        ggplot2::ggtitle(paste("Repayment Rate", repaymentYears, "Years For Different Income Brackets", sep = " "))
+      myPlots
     }
-    plot1
   }
-  
-  myPlots <-  Filter(Negate(is.null), lapply(year, doPlotbyYear))
-  if(length(myPlots) > 1) {
-    numCols <- ceiling(length(myPlots)/2)
-    multiplot(plotlist = myPlots, cols = numCols, byrow=TRUE)
+  else {
+    message("No data is available for requested school(s)")
   }
-  else
-    myPlots
 }
